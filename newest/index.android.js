b636538f4248a7aa05aa4dfd12551074df516287
Neil Advani <neil.advani@vanderbilt.edu>
2016-12-03 23:14:48 -0600

    newest versions

---
+++ newest/index.android.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var React = require('react');
+var ReactNative = require('react-native');
+var postPage = require('./postPage');
+
+ReactNative.AppRegistry.registerComponent('diabetesAppV2', function() { return postPage });
